b84b68c0d9c6d9f59fe4515fca7f8da154a9cc8d
jmin12663 <jmin12663@gmail.com>
2026-05-28 02:11:03 +0900

    fix little

--- src/main/java/com/capstone/gitmanager/auth/repository/PreEmailVerificationRepository.java
+++ src/main/java/com/capstone/gitmanager/auth/repository/PreEmailVerificationRepository.java
@@ -2,10 +2,16 @@ package com.capstone.gitmanager.auth.repository;
 
 import com.capstone.gitmanager.auth.entity.PreEmailVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PreEmailVerificationRepository extends JpaRepository<PreEmailVerification, Long> {
     Optional<PreEmailVerification> findByEmail(String email);
-    void deleteByEmail(String email);
+
+    @Modifying
+    @Query("DELETE FROM PreEmailVerification p WHERE p.email = :email")
+    void deleteByEmail(@Param("email") String email);
 }
